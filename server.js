c314361760c51032de2267d66eaf63b6b24ee7f6
Echo-Path-Coder <Echopath2023@outlook.com>
2026-02-15 12:20:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
 
-
 const app = express();
 app.use(cors());    
 app.use(express.json());
+
+const blockedIPs = []
+
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 5,              // 5 requests per minute for each IP address
+    message: "Too many reports. Please wait."
+});
 
 const WEBHOOK_URL = "https://discord.com/api/webhooks/1471721082003132609/WUEHz65AwlJE0Hz7_G5LIvWZ4-O4oRnH581MGiSdMEO4uTL1dbpCr4EggRitvKeYE0Gr";
 
@@ -17,6 +26,12 @@ app.post("/report", async (req, res) => {
 
     const data = req.body;
 
+    const userIP = req.ip;
+
+    if (blockedIPs.includes(userIP)) {
+        return res.status(403).send("You are blocked from using this website.");
+    }
+    
     //Save to file
     fs.appendFileSync(
         "reports.txt",
@@ -45,4 +60,5 @@ Contact: ${data.contact}`
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
 });
